f34e84ede39591076ee268b20a8c5c955310e5e7
K-JC <kellytolfts93@hotmail.com>
2023-08-29 11:51:09 +0000

    install react router library, set up route paths for home/signin/signup pages, so that when clicked on it takes the user to the correct page, style the navigation a bit more, hover over navagation links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,29 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
+                <NavLink to="/">
                 <Navbar.Brand>
                     <img src={logo} alt="logo" height="45" />
                 </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link>
+                        <NavLink  exact className={styles.NavLink} activeClassName={styles.Active} to="/">
                             <i className="fa-solid fa-house"></i>Home
-                        </Nav.Link>
-                        <Nav.Link>
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
                             <i className="fa-solid fa-right-to-bracket"></i>Sign in
-                        </Nav.Link>
-                        <Nav.Link>
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
                             <i className="fa-sharp fa-solid fa-user-plus"></i>Sign up
-                        </Nav.Link>
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
