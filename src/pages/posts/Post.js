36998c6310e09dd3ddfcbd784c333708f69f4e28
K-JC <kellytolfts93@hotmail.com>
2023-08-31 12:21:08 +0000

    add avatar to the posts page, users post will have the owners profile picture next to it, import avatar/link and card/media

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Card } from "react-bootstrap";
+import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
 
 
 const Post = (props) => {
@@ -28,7 +30,8 @@ const Post = (props) => {
         <Card.Body>
             <Media className="align-items-center justify-content-between">
                 <Link to={`/profiles/${profile_id}`}>
-
+                    <Avatar src={profile_image} height={55} />
+                    {owner}
                 </Link>
             </Media>
         </Card.Body>
