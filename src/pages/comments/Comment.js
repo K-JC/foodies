078ef478f0732d230ef3958df64feef58d18187b
K-JC <kellytolfts93@hotmail.com>
2023-09-03 15:07:23 +0000

    add my comment_like id and comment_like count to comment props, this will give me comment like count for any comments liked. Create a like and unlike component so the comment owner cannot like their own comment. Will increase the like by 1, no duplicate likes and can unlike a comment which will decrease the overall number of likes by 1, use heart icon for the likes to the right of the comment

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
@@ -7,18 +7,22 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { MoreDropdown } from "../../components/MoreDropdown";
 import { axiosRes } from "../../api/axiosDefaults";
 import CommentEditForm from "./CommentEditForm";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
 
 const Comment = (props) => {
-  const { 
+  const {
     profile_id,
     profile_image,
     owner,
     updated_at,
-    content, 
+    content,
     id,
     setPost,
     setComments,
-    } = props;
+    comment_like_id,
+    comment_like_count,
+  } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
@@ -28,6 +32,7 @@ const Comment = (props) => {
    * Edit or delete logged users own comment
    * delete a comment will remove a comment count
    * from total comment count
+   * No post owner can like their own posts/comments
    */
   const handleDelete = async () => {
     try {
@@ -37,7 +42,7 @@ const Comment = (props) => {
           ...prevPost.results[0],
           comments_count: prevPost.results[0].comments_count - 1,
         },
-      ],
+        ],
       }));
       setComments(prevComments => ({
         ...prevComments,
@@ -45,6 +50,67 @@ const Comment = (props) => {
 
       }));
     } catch (err) {
+    }
+  };
+
+  /**
+   * Like a comment made by another user
+   * Unable to like own comment
+   * Increase number of likes by 1
+   *Sends request to foodies api
+   */
+  
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("like_comments/ ", { comment: id });
+      console.log("data", data);
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+              ...comment,
+              comment_like_count
+                : comment.comment_like_count
+                + 1,
+              comment_like_id
+                : data.id,
+            }
+            : comment;
+
+        }),
+      }));
+      console.log("Comments likes count", comment_like_count);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+ /**
+   * Unlike a comment made by another user
+   * Decrease number of likes by 1
+   *Sends request to foodies api
+   */
+  const handleUnlike = async () => {
+    try {
+      await axiosRes.delete(`/like_comments/${comment_like_id}`);
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+              ...comment,
+              comment_like_count: comment.comment_like_count
+                - 1,
+                comment_like_id
+
+                : null,
+            }
+            : comment;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -58,26 +124,48 @@ const Comment = (props) => {
         <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
+          <div className={"text-right"}>
+            {is_owner ? (
+              <OverlayTrigger placement="top" overlay={<Tooltip>You can't like your own comment!</Tooltip>}>
+                <i className="fa-solid fa-heart" />
+              </OverlayTrigger>
+            ) : comment_like_id
+            ? (
+              <span onClick={handleUnlike}>
+                <i className={`fa-solid fa-heart  ${styles.Heart}`} />
+              </span>
+            ) : currentUser ? (
+              <span onClick={handleLike}>
+                <i className={`fa-solid fa-heart ${styles.HeartOutline}`} />
+              </span>
+            ) : (
+              <OverlayTrigger placement="top" overlay={<Tooltip>Please log in to like a post!</Tooltip>}>
+                <i className="fa-solid fa-heart" />
+              </OverlayTrigger>
+            )}
+            {comment_like_count}
+          </div>
+
           {showEditForm ? (
-            <CommentEditForm 
-            id={id}
-            profile_id={profile_id}
-            content={content}
-            profileImage={profile_image}
-            setComments={setComments}
-            setShowEditForm={setShowEditForm}
+            <CommentEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm}
             />
           ) : (
-             <p>{content}</p>
+            <p>{content}</p>
           )}
         </Media.Body>
         {is_owner && !showEditForm && (
-          <MoreDropdown 
-          handleEdit={() => setShowEditForm(true)}
-          handleDelete={handleDelete} />
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete} />
         )}
       </Media>
-      </>
+    </>
   );
 };
 
