93a96b6ce8da48d84c0217e219d5fa8a612c1c06
K-JC <kellytolfts93@hotmail.com>
2023-09-04 19:31:08 +0000

    add profile js file and use racfe snippit. create style sheet for profiles, import it, import buttons and import react. In popular profiles update profile component

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+
+const Profile = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Profile;
