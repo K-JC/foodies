5a2112343b416f5944aa43262b4f9972f067faea
K-JC <kellytolfts93@hotmail.com>
2023-09-05 18:10:31 +0000

    add profile data context js file, profile data sync across all components, make context provider. Move code from popular profiles to profile data context file. Add profile data context to index.js file around the app component. Sort through my imports and get rid of anything I no longer need

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
+import { ProfileDataContext } from "./contexts/ProfileDataContext";
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <CurrentUserProvider>
+        <ProfileDataContext>
         <App />
+        </ProfileDataContext>
       </CurrentUserProvider>
     </Router>
   </React.StrictMode>,
